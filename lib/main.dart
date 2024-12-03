956562a2559fa518060593bb929dd0a34605eaea
Admin <youssefamin2@>
2024-12-03 14:59:48 +0200

    firebase install

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:nfc_project/screens/login_screen/login_screen.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp( const UniversitySystem());
 }
 
@@ -10,7 +17,7 @@ class UniversitySystem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return  MaterialApp(
       debugShowCheckedModeBanner: false,
       home:LoginScreen()
     );
